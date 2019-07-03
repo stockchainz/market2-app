928ff44e065bbe3b0a00f8e3e30ac3bed6759fd2
NickyLogan <laurentiusdominick@yahoo.com>
2019-07-03 21:06:05 +0700

    Updates

---
+++ precache-manifest.8595e6a0d05195b3088bcd9b71a27675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac02c48f73c9ed740dcd3df140cc8cec",
+    "url": "/market2-app/index.html"
+  },
+  {
+    "revision": "2518b44163712fcaaa38",
+    "url": "/market2-app/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "490779ec5d9b42de440a",
+    "url": "/market2-app/static/css/main.6fe004e0.chunk.css"
+  },
+  {
+    "revision": "2518b44163712fcaaa38",
+    "url": "/market2-app/static/js/2.95a6b332.chunk.js"
+  },
+  {
+    "revision": "490779ec5d9b42de440a",
+    "url": "/market2-app/static/js/main.eef4bff0.chunk.js"
+  },
+  {
+    "revision": "9e025fd275096b6c50d8",
+    "url": "/market2-app/static/js/runtime~main.e5a22d4a.js"
+  }
+]);
